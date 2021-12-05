dfa46859035b7562ed1f3b4443f8136b6aa4753c
Tal Livneh <talivneh8@gmail.com>
2021-12-05 16:02:01 +0200

    first commit

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: "04anznm9j62aho1HbkQLhn",
+});
+
+export const playlistState = atom({
+  key: "playlistState",
+  default: null,
+});
